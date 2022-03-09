0e6e1e41ba5c596c9baaecb3d067708e3f1650e5
doom1702 <aniketsingh.1702@gmail.com>
2022-03-09 20:43:03 +0530

    Deploy Commit

--- croprecco/settings.py
+++ croprecco/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6rey#co8n@oa4dyq(dxbujc2ngr#_(jy@t(-==luh1vt*sciwm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["crop-recco.herokuapp.com","127.0.0.1"]
 
